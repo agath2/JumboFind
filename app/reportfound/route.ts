556ca9313b208d77f3cc7317fff476be177504e5
andy <nilydna04@gmail.com>
2026-02-21 16:53:11 -0500

    made reportfound route

---
+++ app/reportfound/route.ts
@@ -0,0 +1,35 @@
+import { NextResponse } from "next/server";
+import { openDb } from "../db";
+
+export async function POST(request: Request) {
+  const contentType = request.headers.get("content-type") ?? "";
+
+  if (contentType.includes("application/json")) {
+    const jsonData = await request.json();
+
+    const id = jsonData.id
+
+    const db = await openDb();
+    await db.run(
+      `UPDATE items 
+      SET found = true
+      WHERE id = ?`,
+      id
+    );
+    await db.close();
+    console.log("Item reported to be found in database");
+
+    return NextResponse.json({
+      ok: true,
+      msg: "item updated.",
+    });
+  } else {
+    return NextResponse.json(
+      {
+        ok: false,
+        msg: "Unsupported content type. Use application/json data.",
+      }, 
+      { status: 415 }
+    );
+  }
+}
