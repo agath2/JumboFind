65b48794aa26fec50a970a6ed5d2af6e4288e031
rdh <98863820+rhysdh540@users.noreply.github.com>
2026-02-22 14:09:20 -0500

    dynamic tag fetching from database

--- app/tags/route.ts
+++ app/tags/route.ts
@@ -3,12 +3,7 @@ import {openDb} from "../db";
 
 export async function GET(request: Request) {
     const db = await openDb();
-
-    const rows = await db.all(`
-        SELECT * FROM tags
-        `);
-    
-    
+    const rows = await db.all(`SELECT * FROM tags`);
     await db.close();
     
     console.log("[report] Searched for all tags in DB");
